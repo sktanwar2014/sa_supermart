201dd0c8a7ac3d3f57183ad6c58edabd97ef3f27
sktanwar2014 <sktanwar.2014@gmail.com>
2020-09-04 09:43:14 +0530

    prod env

--- server/lib/connection.js
+++ server/lib/connection.js
@@ -3,10 +3,18 @@ const MySQL = require("mysql");
 const { env, dbName } = require("./database");
 
 let dbOptions = '';
-let nodePort = 3010;
+let nodePort = 3001;
 
 
 if (env === 'prod') {
+   dbOptions = {
+      host: 'localhost',
+      user: 'a1abil_safresh',
+      password:'K9$X~hgxez;U',
+      port: 3306
+   };
+   nodePort = 3001;
+}else if(env === 'dev'){
    dbOptions = {
       host: 'localhost',
       user: 'root',
--- server/lib/database.js
+++ server/lib/database.js
@@ -1,18 +1,27 @@
-const env = 'local';
+const env = 'prod';
 
-let DbName = 'a1abiliti_shopping';
-let domainName = 'ordernow.a1abilities.co.nz';
-let mailPass = 'q!G#19weS';
-let mailUser = 'admin@ordernow.a1abilities.co.nz';
-let mailService = 'ordernow.a1abilities.co.nz';
+let DbName = 'a1abil_safresh';
+let domainName = 'safresh.a1abilities.com.au';
+let mailPass = 'Pr4m$5ASOU$t';
+let mailUser = 'admin@safresh.a1abilities.com.au';
+let mailService = 'safresh.a1abilities.co.nz';
 
 
 
 console.log('env...', env);
 
 if (env === 'prod') {
+  DbName = 'a1abil_safresh';
+  domainName = 'safresh.a1abilities.com.au';
+  mailPass = 'Pr4m$5ASOU$t';
+  mailUser = 'admin@safresh.a1abilities.com.au';
+  mailService = 'safresh.a1abilities.co.nz';
+}else if(env === 'dev'){
   DbName = 'a1abiliti_shopping';
   domainName = 'ordernow.a1abilities.co.nz';
+  mailPass = 'q!G#19weS';
+  mailUser = 'admin@ordernow.a1abilities.co.nz';
+  mailService = 'ordernow.a1abilities.co.nz';
 } else {  
   DbName = 'a1abiliti_sa_supermart'
   domainName = 'localhost:5000'
